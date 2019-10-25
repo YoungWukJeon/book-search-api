1bad033bcae080dc72ac3794f056c902cce1bbe0
YoungWukJeon <kdh10080@gmail.com>
2019-10-26 07:15:18 +0900

    [FEATURE-1] 사용자 패스워드 암호화를 위한 SpringSecurity의 PasswordEncoder 추가

--- src/main/java/com/edu/booksearch/BookSearchApplication.java
+++ src/main/java/com/edu/booksearch/BookSearchApplication.java
@@ -2,7 +2,9 @@ package com.edu.booksearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {"com.edu.booksearch.util"})
 @SpringBootApplication
 public class BookSearchApplication {
 
---
+++ src/test/java/com/edu/booksearch/util/PasswordEncodingTest.java
@@ -0,0 +1,43 @@
+package com.edu.booksearch.util;
+
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PasswordEncodingTest {
+    private PasswordEncoding passwordEncoding = new PasswordEncoding();;
+
+    @Test
+    void 암호화_인코딩() {
+        String rawPassword = "testPass123!!";
+        String encodedPassword = passwordEncoding.encode(rawPassword);
+
+        System.out.println(rawPassword + " -> " + encodedPassword);
+    }
+
+    @Test
+    void 암호화_비교_같을경우() {
+        String rawPassword1 = "testPass123!!";
+        String encodedPassword1 = passwordEncoding.encode(rawPassword1);
+
+        String rawPassword2 = "testPass123!!";
+        String encodedPassword2 = passwordEncoding.encode(rawPassword2);
+
+        assertEquals(rawPassword1, rawPassword2);
+    }
+
+    @Test
+    void 암호화_비교_다를경우() {
+        String rawPassword1 = "testPass123!!";
+        String encodedPassword1 = passwordEncoding.encode(rawPassword1);
+
+        String rawPassword2 = "testPass125!!";
+        String encodedPassword2 = passwordEncoding.encode(rawPassword2);
+
+        System.out.println(rawPassword1+ " -> " + encodedPassword1);
+        System.out.println(rawPassword2+ " -> " + encodedPassword2);
+
+        assertEquals(rawPassword1, rawPassword2);
+    }
+}
